d7d6b6a3d4b46cbfdbc3865836374e1f5862b014
Jazmin <programajazminc20@gmail.com>
2025-11-16 12:53:26 +0100

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+        int digitos = 0;
+        int volteado = 0;
+        boolean aparece = false;
+
+        while (num > 0) {
+            digitos = num % 10;
+            volteado = (volteado * 10) + (digitos);            
+            num = num / 10;
+        }
+        System.out.print("Dígitos que aparecen en el número: ");
+
+        while (volteado > 0) {
+            digitos = volteado % 10;
+            for (int i = 0; i <= 10; i++) {
+                if (digitos == i) {
+                    System.out.print(i + " ");
+                    aparece = true;
+                }else {
+                    System.out.print(digitos + " ");
+                }
+            volteado = volteado / 10;
+            }
+        }
     }
 }
